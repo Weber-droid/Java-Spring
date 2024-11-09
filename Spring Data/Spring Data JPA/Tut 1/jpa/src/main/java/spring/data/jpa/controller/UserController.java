bcf7f0492c840dfbfce662517490910caadb6ab9
Emmanuel Tetteh Totimeh <emmanueltotimeh2002@gmail.com>
2024-11-09 21:24:21 +0000

    Bug-fixes

--- Spring Data/Spring Data JPA/Tut 1/jpa/src/main/java/spring/data/jpa/controller/UserController.java
+++ Spring Data/Spring Data JPA/Tut 1/jpa/src/main/java/spring/data/jpa/controller/UserController.java
@@ -2,6 +2,7 @@ package spring.data.jpa.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,13 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(Long id){
+        return userService.getUserById(id)
+                          .map(ResponseEntity::ok)
+                          .orElse(ResponseEntity.notFound().build());
     }
 
     
