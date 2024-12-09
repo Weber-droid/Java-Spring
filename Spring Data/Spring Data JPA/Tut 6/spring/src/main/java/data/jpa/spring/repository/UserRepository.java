307dd09370dd9f5ebffee950d7d3edf8ab4479cf
emmanueltotimeh2002 <emmanueltotimeh2002@gmail.com>
2024-12-09 02:42:49 +0000

    Bug fixes

---
+++ Spring Data/Spring Data JPA/Tut 6/spring/src/main/java/data/jpa/spring/repository/UserRepository.java
@@ -0,0 +1,9 @@
+ package data.jpa.spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import data.jpa.spring.entity.User;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+
+}
